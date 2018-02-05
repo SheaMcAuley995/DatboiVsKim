f2ce86bc82e6b6a7858f23c6aa207b9d3c58483b
Wyatt Gallagher <s179053@students.aie.edu.au>
2018-02-05 15:26:11 -0800

    started working on scripts for ugandan knuckles

---
+++ Assets/_WyattAssets/_Scripts/WanderScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WanderScript : MonoBehaviour
+{
+    NavMeshAgent knuckles;
+
+    public float speed;
+    public float radius;
+    public float jitter;
+    public float distance;
+    public Vector3 target;
+    public Rigidbody rb;
+
+
+    void Start()
+    {
+        knuckles = GetComponent<NavMeshAgent>();
+    }
+
+
+    public void Update()
+    {
+
+        knuckles.destination = (transform.position + returnWanderPoints());
+    }
+
+
+    public Vector3 returnWanderPoints()
+    {
+        target = Vector3.zero;
+        target = Random.insideUnitCircle.normalized * radius;
+        target = (Vector2)target + Random.insideUnitCircle * jitter;
+        target.z = target.y;
+        target.y = 0;
+        target += transform.position;
+        target += transform.forward * distance;
+        Vector3 dir = (target - transform.position).normalized;
+        Vector3 desiredVel = dir * speed;
+        return target;
+    }
+}
